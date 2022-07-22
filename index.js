27df10d8301c8f2d6b24f1c4520f4cb853b55334
DonAlejandro01 <alejandrod.perezn@gmail.com>
2022-07-22 14:28:49 -0400

    22/07
    Incluimos la explicacion de flex row y como alinear los items

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 /*Para que funcione el button se debe declarar el id en el html para que pueda ser identificado por el JS */
 const alingEnd=()=>{
-    document.getElementById("container").style.alignItems = "flex-end";
+    document.getElementById("container-colum").style.alignItems = "flex-end";
 }
 
 const alingStart=()=>{
-    document.getElementById("container").style.alignItems = "flex-start";
+    document.getElementById("container-colum").style.alignItems = "flex-start";
 }
 
 const alingCenter =()=>{
-    document.getElementById("container").style.alignItems = "center";
+    document.getElementById("container-colum").style.alignItems = "center";
+}
+
+/*Main Row */
+
+const RowStart=()=>{
+    document.getElementById("container-Row").style.justifyContent = "flex-start";
+
+}
+
+const RowCenter = () =>{
+    document.getElementById("container-Row").style.justifyContent = "center";
+
+}
+
+const RowEnd = () =>{
+    document.getElementById("container-Row").style.justifyContent = "flex-end";
 }
