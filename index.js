7c8db5ce2de73afddd5a24ca8ba72857b9b50054
DonAlejandro01 <alejandrod.perezn@gmail.com>
2022-07-22 12:36:45 -0400

    22/07
    Practicando, nuevamente, flex y sus variantes. ademas asigne un botom para que cambie la posicion del contenedor para entender como funciona flex y aling item con manipulacion del dom

---
+++ index.js
@@ -0,0 +1,12 @@
+/*Para que funcione el button se debe declarar el id en el html para que pueda ser identificado por el JS */
+const alingEnd=()=>{
+    document.getElementById("container").style.alignItems = "flex-end";
+}
+
+const alingStart=()=>{
+    document.getElementById("container").style.alignItems = "flex-start";
+}
+
+const alingCenter =()=>{
+    document.getElementById("container").style.alignItems = "center";
+}
